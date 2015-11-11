e14b3e0c2f5aef90152a2fcea667181f521e0bd2
ljnazar <ljnazar@gmail.com>
2015-11-11 15:44:36 -0300

    Code optimization

--- js/functions.js
+++ js/functions.js
@@ -1,168 +1,114 @@
-var navegador="#nav1";
-var lenguaje="#ESP";
-var indicador="#i1";
-var navleng="#navES";
+var navegador="nav1";
+var lenguaje="ESP";
+var indicador="i1";
 
 
-//evento al colocar el puntero del raton sobre la imagen
-$('#i1').hover(function(){
-	//buscamosla imagen y la rotamos 360 grados
-	$('#i1').stop(true,true).rotate({
-		duration: 3000,
-		animateTo: 360
-	});
-}, //evento al retirar el raton del elemento
-function(){
-	//rotamos la imagen a 0 (cero) grados
-	$('#i1').stop(true,true).rotate({
-		animateTo: 0
-	});
-});
-//evento al colocar el puntero del raton sobre la imagen
-$('#i2').hover(function(){
-	//buscamosla imagen y la rotamos 360 grados
-	$('#i2').stop(true,true).rotate({
-		duration: 3000,
-		animateTo: 360
-	});
-}, //evento al retirar el raton del elemento
-function(){
-	//rotamos la imagen a 0 (cero) grados
-	$('#i2').stop(true,true).rotate({
-		animateTo: 0
-	});
-});
-//evento al colocar el puntero del raton sobre la imagen
-$('#i3').hover(function(){
-	//buscamosla imagen y la rotamos 360 grados
-	$('#i3').stop(true,true).rotate({
-		duration: 3000,
-		animateTo: 360
-	});
-}, //evento al retirar el raton del elemento
-function(){
-	//rotamos la imagen a 0 (cero) grados
-	$('#i3').stop(true,true).rotate({
-		animateTo: 0
-	});
-});
+//código a ejecutar cuando el DOM está cargado completamente.
+$(document).ready(function() {
 
+	//evento al colocar el puntero del raton sobre la imagen
+	$('#i1').hover(function(){
+		//buscamosla imagen y la rotamos 360 grados
+		$('#i1').stop(true,true).rotate({
+			duration: 3000,
+			animateTo: 360
+		});
+	}, //evento al retirar el raton del elemento
+	function(){
+		//rotamos la imagen a 0 (cero) grados
+		$('#i1').stop(true,true).rotate({
+			animateTo: 0
+		});
+	});
+	//evento al colocar el puntero del raton sobre la imagen
+	$('#i2').hover(function(){
+		//buscamosla imagen y la rotamos 360 grados
+		$('#i2').stop(true,true).rotate({
+			duration: 3000,
+			animateTo: 360
+		});
+	}, //evento al retirar el raton del elemento
+	function(){
+		//rotamos la imagen a 0 (cero) grados
+		$('#i2').stop(true,true).rotate({
+			animateTo: 0
+		});
+	});
+	//evento al colocar el puntero del raton sobre la imagen
+	$('#i3').hover(function(){
+		//buscamosla imagen y la rotamos 360 grados
+		$('#i3').stop(true,true).rotate({
+			duration: 3000,
+			animateTo: 360
+		});
+	}, //evento al retirar el raton del elemento
+	function(){
+		//rotamos la imagen a 0 (cero) grados
+		$('#i3').stop(true,true).rotate({
+			animateTo: 0
+		});
+	});
+
+
+function clear_container(){
+	$('#lala').css('display','none');
+	$('.list').css('display','none');
+}
 
 function limpiar(){
 	$('#nav1').removeClass('active');
 	$('#nav2').removeClass('active');
 	$('#nav3').removeClass('active');
 	$('#nav4').removeClass('active');
-	$('#nav5').removeClass('active');
-	$('#nav3').removeClass('active');
-	$('#nav6').removeClass('active');
-	$('#nav7').removeClass('active');
-	$('#nav8').removeClass('active');
 }
 
 function reseteo(){
-	indicador="#i1";
+	indicador="i1";
 	$('#i1').attr('src', 'img/ind-act.png');
 	$('#i2').attr('src', 'img/ind-des.png');
 	$('#i3').attr('src', 'img/ind-des.png');
 }
 
 $('#nav1').click(function(){
-	navegador="#nav1";
+	navegador="nav1";
 	limpiar();
 	reseteo();
 	$('#nav1').addClass('active');
-	$('#nav5').addClass('active');
 	$('.ind').css('display','block');
 	evaluar();
 });
 
 $('#nav2').click(function(){
-	navegador="#nav2";
+	navegador="nav2";
 	limpiar();
 	reseteo();
 	$('#nav2').addClass('active');
-	$('#nav6').addClass('active');
 	$('.ind').css('display','block');
 	evaluar();
 });
 
 $('#nav3').click(function(){
-	navegador="#nav3";
+	navegador="nav3";
 	limpiar();
 	reseteo();
 	$('#nav3').addClass('active');
-	$('#nav7').addClass('active');
 	$('.ind').css('display','none');
 	evaluar();
 });
 
 $('#nav4').click(function(){
-	navegador="#nav4";
+	navegador="nav4";
 	limpiar();
 	reseteo();
 	$('#nav4').addClass('active');
-	$('#nav8').addClass('active');
 	$('.ind').css('display','none');
 	evaluar();
 });
 
-$('#nav5').click(function(){
-	navegador="#nav5";
-	limpiar();
-	reseteo();
-	$('#nav1').addClass('active');
-	$('#nav5').addClass('active');
-	$('.ind').css('display','block');
-	evaluar();
-});
-
-$('#nav6').click(function(){
-	navegador="#nav6";
-	limpiar();
-	reseteo();
-	$('#nav2').addClass('active');
-	$('#nav6').addClass('active');
-	$('.ind').css('display','block');
-	evaluar();
-});
-
-$('#nav7').click(function(){
-	navegador="#nav7";
-	limpiar();
-	reseteo();
-	$('#nav3').addClass('active');
-	$('#nav7').addClass('active');
-	$('.ind').css('display','none');
-	evaluar();
-});
-
-$('#nav8').click(function(){
-	navegador="#nav8";
-	limpiar();
-	reseteo();
-	$('#nav4').addClass('active');
-	$('#nav8').addClass('active');
-	$('.ind').css('display','none');
-	evaluar();
-});
 
 
 $('#ESP').click(function(){
-	lenguaje="#ESP";
-	navleng="#navES";
-	if(navegador=="#nav5"){
-		navegador="#nav1";
-	}
-	else if(navegador=="#nav6"){
-		navegador="#nav2";
-	}
-	else if(navegador=="#nav7"){
-		navegador="#nav3";
-	}
-	else{//if(navegador=="#nav8")
-		navegador="#nav4";
-	}
+	lenguaje="ESP";
 	$('#ESP').addClass('active');
 	$('#ENG').removeClass('active');
 	$('#shield').css('margin-left', '16px');
@@ -170,20 +116,7 @@ $('#ESP').click(function(){
 });
 
 $('#ENG').click(function(){
-	lenguaje="#ENG";
-	navleng="#navEN";
-	if(navegador=="#nav1"){
-		navegador="#nav5";
-	}
-	else if(navegador=="#nav2"){
-		navegador="#nav6";
-	}
-	else if(navegador=="#nav3"){
-		navegador="#nav7";
-	}
-	else{//if(navegador=="#nav4")
-		navegador="#nav8";
-	}
+	lenguaje="ENG";
 	$('#ENG').addClass('active');
 	$('#ESP').removeClass('active');
 	$('#shield').css('margin-left', '77px');
@@ -192,7 +125,7 @@ $('#ENG').click(function(){
 
 
 $('#i1').click(function(){
-	indicador="#i1";
+	indicador="i1";
 	$('#i1').attr('src', 'img/ind-act.png');
 	$('#i2').attr('src', 'img/ind-des.png');
 	$('#i3').attr('src', 'img/ind-des.png');
@@ -200,7 +133,7 @@ $('#i1').click(function(){
 });
 
 $('#i2').click(function(){
-	indicador="#i2";
+	indicador="i2";
 	$('#i2').attr('src', 'img/ind-act.png');
 	$('#i1').attr('src', 'img/ind-des.png');
 	$('#i3').attr('src', 'img/ind-des.png');
@@ -208,7 +141,7 @@ $('#i2').click(function(){
 });
 
 $('#i3').click(function(){
-	indicador="#i3";
+	indicador="i3";
 	$('#i3').attr('src', 'img/ind-act.png');
 	$('#i1').attr('src', 'img/ind-des.png');
 	$('#i2').attr('src', 'img/ind-des.png');
@@ -217,74 +150,94 @@ $('#i3').click(function(){
 
 
 function evaluar(){
-	$('.jumbotron').css('display','none');
 
-	if(navleng=='#navES'){
-		$('#navES').css('display','block');
-		$('#navEN').css('display','none');
-	}
-	else{
-		$('#navES').css('display','none');
-		$('#navEN').css('display','block');
-	}
+	clear_container();
 
-	if(navegador=='#nav1'){
-		if(indicador=='#i1'){
-			$('#nav1-1').css('display','block');
-		}
-		else if(indicador=='#i2'){
-			$('#nav1-2').css('display','block');
-		}
-		else{//if(indicador=='#i3')
-			$('#nav1-3').css('display','block');
-		}
-	}
-	else if(navegador=='#nav2'){
-		if(indicador=='#i1'){
-			$('#nav2-1').css('display','block');
-		}
-		else if(indicador=='#i2'){
-			$('#nav2-2').css('display','block');
-		}
-		else{//if(indicador=='#i3')
-			$('#nav2-3').css('display','block');
-		}
-	}
-	else if(navegador=='#nav3'){
-		$('#nav3-1').css('display','block');
-	}
-	else if(navegador=='#nav4'){
-		$('#nav4-1').css('display','block');
-	}
-	else if(navegador=='#nav5'){
-		if(indicador=='#i1'){
-			$('#nav5-1').css('display','block');
-		}
-		else if(indicador=='#i2'){
-			$('#nav5-2').css('display','block');
-		}
-		else{//if(indicador=='#i3')
-			$('#nav5-3').css('display','block');
-		}
-	}
-	else if(navegador=='#nav6'){
-		if(indicador=='#i1'){
-			$('#nav6-1').css('display','block');
-		}
-		else if(indicador=='#i2'){
-			$('#nav6-2').css('display','block');
-		}
-		else{//if(indicador=='#i3')
-			$('#nav6-3').css('display','block');
-		}
-	}
-	else if(navegador=='#nav7'){
-		$('#nav7-1').css('display','block');
-	}
-	else{//if(navegador=='#nav8')
-		$('#nav8-1').css('display','block');
-	}
+  //obtengo un objeto llamado json con toda la estructura (Lo obtiene de forma asíncrona)
+  //Buscar la forma de hacerlo sin que sea asíncrono, para que no funcione lento.
+  $.getJSON('../data.json', function(json) {
+
+  	var context;
+  	var context2;
+
+
+  	if(lenguaje=="ESP"){
+  		context=json.es.titles;
+  		if(navegador=="nav1"){
+			if(indicador=="i1"){
+				context2=json.es.datos_nav1_i1;
+			}
+			else if(indicador=="i2"){
+				context2=json.es.datos_nav1_i2;
+				$('.list').css('display','block');
+			}
+			else{//if(indicador=="i3")
+				
+			}
+  		}
+  		else if(navegador=="nav2"){
+			if(indicador=="i1"){
+
+			}
+			else if(indicador=="i2"){
+				
+			}
+			else{//if(indicador=="i3")
+				
+			}
+  		}
+  		else if(navegador=="nav3"){
+  			
+  		}
+  		else{//if(navegador=="nav4")
+
+  		}
+    }
+    else{//if(lenguaje=="ENG")
+  		context=json.en.titles;
+  	  	if(navegador=="nav1"){
+			if(indicador=="i1"){
+				context2=json.en.datos_nav1_i1;
+			}
+			else if(indicador=="i2"){
+			}
+			else{//if(indicador=="i3")
+				
+			}
+  		}
+  		else if(navegador=="nav2"){
+			if(indicador=="i1"){
+
+			}
+			else if(indicador=="i2"){
+				
+			}
+			else{//if(indicador=="i3")
+				
+			}
+  		}
+  		else if(navegador=="nav3"){
+  			
+  		}
+  		else{//if(navegador=="nav4")
+
+  		}
+    }
+
+	$('#titles').loadJSON(context);
+
+	$('#data').loadJSON(context2);
+
+	$('.list').loadJSON(list);
+
+
+
+  });
 	
 }
 
 evaluar();
+
+
+
+});
